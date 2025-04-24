d661b7c9837c8be85a961c6f2e5d9d7da5962976
ardenaAfif <aafifpratama@gmail.com>
2025-04-24 21:11:43 +0700

    [Afif] -Refactor: Hide berita fitur

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "id.ardev.keretakita"
         minSdk = 21
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

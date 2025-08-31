a35c81e53fffda4ff65c9af381ec438de107586f
ardenaAfif <aafifpratama@gmail.com>
2025-08-31 07:45:39 +0700

    Updated version code and name, added privacy policy content, and removed unnecessary strings.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "id.ardev.keretakita"
         minSdk = 21
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.0.3"
+        versionCode = 6
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
